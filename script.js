49c2a784b7aababce6fa6b3863eb66c2e13e7635
pratikbhattarai76 <pratikbhattarai76@gmail.com>
2026-07-01 00:21:46 +0545

    feat: created javascript functions

---
+++ script.js
@@ -0,0 +1,28 @@
+const navbar = document.getElementById('navbar');
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu');
+
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY > 0){
+        navbar.classList.add('navbar-styling');
+    }
+
+    else{
+        navbar.classList.remove('navbar-styling');
+    }
+})
+
+burger.addEventListener('click' , ()=>{
+    menu.classList.toggle('translate-x-0');
+})
+
+
+
+ var typed = new Typed('#typing', {
+      strings: ['Developer', 'Singer', 'Traveller', 'Coder' ],
+      typeSpeed: 100,
+      loop: true,
+      backSpeed: 100,
+      backDelay:600,
+      showCursor:false,
+    });
