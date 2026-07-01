632347f4bc76f81273c07a6e33f61dbe99eca83a
pratikbhattarai76 <pratikbhattarai76@gmail.com>
2026-07-01 09:54:49 +0545

    chore: fixed spacing in recent work headers

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ const navbar = document.getElementById('navbar');
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
 
-document.addEventListener('scroll', ()=>{
-    if(window.scrollY > 0){
+document.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
         navbar.classList.add('navbar-styling');
     }
 
-    else{
+    else {
         navbar.classList.remove('navbar-styling');
     }
 })
 
-burger.addEventListener('click' , ()=>{
+burger.addEventListener('click', () => {
     menu.classList.toggle('translate-x-0');
 })
 
 
 
- var typed = new Typed('#typing', {
-      strings: ['Developer', 'Singer', 'Traveller', 'Coder' ],
-      typeSpeed: 100,
-      loop: true,
-      backSpeed: 100,
-      backDelay:600,
-      showCursor:false,
-    });
+var typed = new Typed('#typing', {
+    strings: ['Developer', 'Singer', 'Traveller', 'Coder'],
+    typeSpeed: 100,
+    loop: true,
+    backSpeed: 100,
+    backDelay: 600,
+    showCursor: false,
+});
